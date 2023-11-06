2602f0617ee2085c9357fe26ad955524106fa37a
Matheus <matheu.mpf18@gmail.com>
2023-11-05 22:02:24 -0300

    Commit 2.0 > Generating administrative screen

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import "./app.css"
 import Book from "./imgs/book.png"
 import Author from "./imgs/author.png"
 import BookPublisher from "./imgs/bookPublisher.png"
+import Category from "./imgs/category.png"
 
 function App() {
   return (
-    <div className="App">
-      <Cards info="Livros" srcImg={Book} altImg="Imagem de livro" description="Cadastre, liste e busque os livros aqui" />
-      <Cards info="Autor" srcImg={Author} altImg="Imagem de autor genérico" description="Cadastre, liste e busque os autores aqui" />
-      <Cards info="Editora" srcImg={BookPublisher} altImg="Imagem de lápis" description="Cadastre, liste e busque as editoras aqui" />
-    </div>
+
+    <main>
+      <ul class="card-listing">
+        <Cards info="Livros" srcImg={Book} altImg="Imagem de livro" description="Cadastre, liste e busque os livros aqui" />
+        <Cards info="Autor" srcImg={Author} altImg="Imagem de autor genérico" description="Cadastre, liste e busque os autores aqui" />
+        <Cards info="Editora" srcImg={BookPublisher} altImg="Imagem de lápis" description="Cadastre, liste e busque as editoras aqui" />
+        <Cards info="Categoria" srcImg={Category} altImg="Imagem de marcador" description="Cadastre, liste e busque as categorias aqui" />
+      </ul>
+    </main>
+
   );
 }
 
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -3,19 +3,15 @@ import "./Cards.css"
 const Cards = (props) => {
     return (
 
-        <ul className="card-listing">
-
             <li className="card">
                 <div className="card-info">
-                    <span>{props.info}</span>
+                    <span className="span-title-card">{props.info}</span>
                 </div>
 
-                <img src={props.srcImg} alt={props.altImg} />
+                <img className="img-card" src={props.srcImg} alt={props.altImg} />
 
                 <p className="description">{props.description}</p>
             </li>
-
-        </ul>
     )
 
 }
